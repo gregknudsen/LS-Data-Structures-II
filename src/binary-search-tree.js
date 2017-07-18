d3d8f8b66ce7da430c2885e3a14c9f813e3d0308
Antonio Melendez <antoniomelendez@Antonios-MacBook-Pro.local>
2017-07-18 15:37:03 -0600

    Binary Search Tree - Complete

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,9 +1,43 @@
 // https://msdn.microsoft.com/en-us/library/aa289150(v=vs.71).aspx
+/* #### Binary Search Tree
+
+  * Should have the methods: `insert`, `contains`, and `depthFirstForEach`.
+  * `insert(value)` inserts the new value at the correct location in the tree.
+  * `contains(value)` searches the tree and returns `true` if the the tree contains the specified value.
+  * `depthFirstForEach(cb)` should iterate over the tree using DFS and passes each node of the tree to the given callback function.
+*/
 class BinarySearchTree {
   constructor(value) {
     this.value = value;
     this.left = null;
     this.right = null;
+    this.container = [value];
+  }
+  insert(value) {
+    this.container.push(value);
+    const node = new BinarySearchTree(value);
+    if (value <= this.value) {
+      if (!this.left) this.left = node;
+      else this.left.insert(value);
+    } else if (value > this.value) {
+      if (!this.right) this.right = node;
+      else this.right.insert(value);
+    }
+  }
+  contains(value) {
+    if (this.value === value) {
+      return true;
+    } else if (value > this.value) {
+      if (this.right) {
+        return this.right.contains(value);
+      }
+      return false;
+    }
+  }
+  depthFirstForEach(cb) {
+    return this.container.forEach((i) => {
+      cb(i);
+    });
   }
 }
 
--- src/tree.js
+++ src/tree.js
@@ -1,7 +1,30 @@
+/*
+#### Trees
+
+  * Should have the methods: `addChild`, and `contains`
+  * Each node on the tree should have a `value` property and a `children` array.
+  * `addChild(value)` should accept a value and add it to that node's `children` array.
+  * `contains(value)` should return `true` if the tree or its children the given value.
+  * When you add nodes to the `children` array use `new Tree(value)` to create the node.
+  * You can instantiate the `Tree` class inside of itself.
+*/
 class Tree {
   constructor(value) {
     this.value = value;
     this.children = [];
+  }
+  addChild(value) {
+    const newNode = new Tree(value);
+    return this.children.push(newNode);
+  }
+  contains(value) {
+    if (this.value === value) {
+      return true;
+    }
+    for (let i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(value)) return true;
+    }
+    return false;
   }
 }
 
